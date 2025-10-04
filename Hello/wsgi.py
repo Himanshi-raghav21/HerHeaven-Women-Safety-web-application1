befc52bfee678f43f1110b0795951418840256ee
Himanshi Raghav <himanshiraghav21@gmail.com>
2025-10-04 23:54:48 +0530

    Update wsgi.py

--- Hello/wsgi.py
+++ Hello/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import django
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hello.settings')
+
+#new added
+django.setup()
+call_command("migrate", interactive=False)
 
 application = get_wsgi_application()
